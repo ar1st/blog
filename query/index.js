7ade25b81b25e8af79a03ca6b73e163719d38034
Aristeidis Tsachlaris <aristnm@hotmail.com>
2021-11-27 13:52:55 +0200

    Initialize query service

---
+++ query/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+
+})
+
+app.post('/events', (req, res) => {
+
+})
+
+
+app.listen(4002, () => {
+    console.log('Listening on 4002')
+})
+
